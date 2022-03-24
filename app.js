a76fed29a197a39d5dbfcc4ee214a8fddd89c577
Damini Pandey <61384878+DaminiPandey@users.noreply.github.com>
2022-03-24 12:51:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https=require("https");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+})
+app.post("/", function(req,res){
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+    const email=req.body.email;
+    console.log(firstName,lastName,email);
+    const data={
+        members:[
+            {
+                email_address: email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }     
+            }
+        ]
+    };
+    app.post("/failure",function(req,res){
+        res.redirect("/");
+    })
+    const jsonData=JSON.stringify(data);
+    const url="https://us6.api.mailchimp.com/3.0/lists/80273ca378";
+    const options = {
+        method:"POST",
+        auth:"damini1:eb1f068062dcb9e01e25b895f0180ad8-us6"
+    }
+    
+const request = https.request(url, options, function(response) {
+    if(response.statusCode===200){
+        res.sendFile(__dirname + "/success.html");
+    }
+    else{
+        res.sendFile(__dirname+ "/failure.html")
+    }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+app.listen(3000,function(){
+    console.log("App is running on port 3000");
+});
+
+
+// mailChimp API key
+// eb1f068062dcb9e01e25b895f0180ad8-us6 
+// List Id
+// 80273ca378
